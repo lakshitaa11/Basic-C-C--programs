31c35cdc4bfb804b95271af71f1b16a951b5170f
lakshitaa11 <97162780+lakshitaa11@users.noreply.github.com>
2022-09-18 22:37:56 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    int sum = a + b;
+
+    cout<<"Sum of two numbers is:"<< sum <<"\n";
+    return 0;
+}
+
